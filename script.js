b60e013154a90bf260633ea3a599d729bd3bad66
soumyadip <soumyadipsahoo2006gmail.com>
2026-09-24 22:34:26 +0530

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-document.querySelector("")
+var btn = document.querySelector("button")
+var persent = document.querySelector("#persent")
+var growth = document.querySelector("#growth")
+var grow = 0;
+
+btn.addEventListener("click", function () {
+var int  =  setInterval(function () {
+        console.log(grow);
+
+        grow++
+        persent.innerHTML = grow + "%"
+        growth.style.width = grow + "%"
+
+    }, 100)
+    setTimeout(() => {
+        clearInterval(int)
+        btn.innerHTML = "Downloaded File"
+        btn.style.opacity = 0.5;
+    }, 10000);
+}
